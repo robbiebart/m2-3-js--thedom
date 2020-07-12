8423d95189582ae8547a48147da241c18bce439b
Robbie Bart <adm@AdminisatorsAir.lan>
2020-07-12 17:27:15 -0400

    mvp

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,30 +1,33 @@
 // Add your code here!
 
-const mainDiv = document.querySelector('main')
+const mainDiv = document.querySelector("main");
 
-const h1 = document.createElement('h1');
-const p1 = document.createElement('p');
-const h2 = document.createElement('h2');
-const p2 = document.createElement('p');
-const image = document.createElement('img');
-const p3 = document.createElement('p');
-const p4 = document.createElement('p');
-const source = document.createElement('a');
+const h1 = document.createElement("h1");
+const p1 = document.createElement("p");
+const h2 = document.createElement("h2");
+const p2 = document.createElement("p");
+const image = document.createElement("img");
+const p3 = document.createElement("p");
+const p4 = document.createElement("p");
+const source = document.createElement("a");
 
+h1.innerText = "The best How I Met Your Mother episode (according to fans)";
+p1.innerText =
+  "As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…";
+h2.innerText = "The Slap Bet (Season 2, Episode 9)";
+p2.innerText = "IMDB Rating: 9.5";
+image.src = "./images/robin-sparkles.jpg";
+image.alt = "Robin Sparkles";
+p3.innetText =
+  "In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.";
+p4.innerText =
+  "In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).";
+source.href =
+  "https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/";
+source.innerText = "Source";
 
-h1.innerText = 'The best How I Met Your Mother episode (according to fans)';
-p1.innerText = 'As impossible as it seems to name just 1 of the 208 legendary episodes as the “best”, the fans have spoken and the highest rated episode is… wait for it…'
-h2.innerText = 'The Slap Bet (Season 2, Episode 9)'
-p2.innerText = 'IMDB Rating: 9.5'
-image.src = './images/robin-sparkles.jpg';
-image.alt = 'Robin Sparkles';
-p3.innetText = 'In this episode, Ted is learning a few secrets about Robin, namely that she has an unexplained aversion to the mall. Robin refuses to tell Ted and the others why she won’t go to the mall, so the gang forms their own theories. Marshall believes it’s because she got married in a mall and is still married. Barney believes it has something to do with Robin having performed in a porn video. The two of them make a slap bet with each other: whoever is right gets to slap the other across the face as hard as he can. Lily is named the Slap Bet Commissioner, as long as she promises to be unbiased.'
-p4.innerText = 'In the end, Robin’s big secret was revealed- she was a Canadian, teen pop sensation named Robin Sparkles. Barney finds the music video for her hit single “Lets Go To The Mall”. The slap bet takes a few turns throughout the episode, but ends with Marshall having the right to slap Barney 5 times- available for all of eternity (horrible call Barney).'
-source.href = 'https://himym2017.wordpress.com/2017/03/25/the-best-himym-episode-according-to-fans/';
-source.innerText = 'Source';
-
-mainDiv.appendChild(h1); 
-mainDiv.appendChild(p1); 
+mainDiv.appendChild(h1);
+mainDiv.appendChild(p1);
 mainDiv.appendChild(h2);
 mainDiv.appendChild(p2);
 mainDiv.appendChild(image);
@@ -32,8 +35,7 @@ mainDiv.appendChild(p3);
 mainDiv.appendChild(p4);
 mainDiv.appendChild(source);
 
-const stylesheet = document.createElement('link');
-stylesheet.rel = 'stylesheet';
-stylesheet.href = 'styles.css';
+const stylesheet = document.createElement("link");
+stylesheet.rel = "stylesheet";
+stylesheet.href = "styles.css";
 document.head.appendChild(stylesheet);
-
--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -4,8 +4,82 @@ const FROGS = 3;
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
 
+const ol = document.getElementById("track");
+
+const stylesheet = document.createElement("link");
+stylesheet.rel = "stylesheet";
+stylesheet.href = "../assets/styles.css";
+stylesheet.type = "text/css";
+document.head.appendChild(stylesheet);
+console.log(stylesheet);
+/* 
+the document element only exists on the browser
+the document is the screen of the browser, the space that has the html tags you're looking for
+now that when you do ol.innerText, you
+anytime you do document.getElementBy... anything you get back will be an HTML tag
+once you have targeted the HTML and put it in a variable, you can use the variable to target it
+and assign new attributes to it that is valid for that kind of tag, for example
+*/
+console.log(frogStable);
+
+for (let i = 0; i < FROGS; i++) {
+  const li = document.createElement("li");
+  li.id = `lane-${i + 1}`;
+  const span = document.createElement("span");
+  span.innerText = `${i + 1}`;
+  ol.appendChild(li);
+  li.appendChild(span);
+}
+
+let racers = [];
+
+for (let i = 0; i < FROGS; i++) {
+  // let number = make a random nmber, pass it on instead of i; just make sure this function returns a number between 0-4
+  racers.push(frogStable[i]);
+}
+// after this, use math.random to select a number form 1 to 4, and pass that instead
+
+racers.forEach((racer, index) => {
+  const li = document.getElementById(`lane-${index + 1}`);
+  const frogSpan = document.createElement("span");
+  frogSpan.classList.add("frog");
+  frogSpan.innerText = racer.number;
+  frogSpan.style.backgroundColor = racer.color;
+  li.appendChild(frogSpan);
+  racer.progress = 0;
+});
+
+console.log(racers);
+
+/*
+the way we're going to make the frog jump is by changing left of the frog, which is curren
+tly at the leftmost of the track; every time we change the frog, it'll go a bit further
+we'll write a function that randomly creates a new number, and that's the value 
+i'll give it to frog, so frog.style.left = new random number
+in order to know where to hop, you need to know where you are right now
+let's say if you don't have progress and your number is chosen randomly = 0-10, and you assign
+frog distance to that, and then frogstyle.left = interval, and then store that in progress
+so set interval will run function that makes it hop, and inside that function is where you
+update your racer.progress, so that the next hop, you work on your last place and move from 
+there
+you store the hop in progress, and in the next hop you do progress + next hop
+*/
+
+// in js objects are dynamic, you can add properties to objects at any time
+// in stricter programming languages you'd have to change the class to change the obj
+// that's what we mean by js being a dynamic language
+
 // 2. Create li
 
 // 3. Create span and add it to the li
 
 // 4. Assign an id to each lane
+
+/*
+the list items and span is just to show the track on the screen
+the reason we need a loop is to set up three tracks
+at the beginning of the loop, we also insert the track number
+    each lane will have an indidivual frog, so we need each li to have a unique id, to target them
+    in future we will use bracket notation to put stuff in there
+    once we do document.getElementByID its going to usethe unique ids
+*/
